be0b3943bf53f1b334b0d5cb48bbac78a82735b1
BijianWu <384318185@qq.com>
2024-09-25 00:06:45 +0100

    export react data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { ExportToExcel } from './ExportToExcel';
+
 function App() {
+  const [data, setData] = useState([]);
+  const fileName = "myfile"; // here enter filename for your excel file
+  useEffect(() => {
+      const fetchData = async () => {
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then(response => setData(response.data));
+      }
+      fetchData();
+  });
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExportToExcel  apiData={data} fileName={fileName} />
     </div>
   );
 }
