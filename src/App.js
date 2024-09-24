75b165e4e234e363bc8526b266b92cec8db1b185
BijianWu <384318185@qq.com>
2024-09-25 00:16:02 +0100

    now able to take in json file as input

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import axios from 'axios';
 import { ExportToExcel } from './ExportToExcel';
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const fileName = "myfile"; // here enter filename for your excel file
-  useEffect(() => {
-      const fetchData = async () => {
-          axios.get('https://jsonplaceholder.typicode.com/posts')
-          .then(response => setData(response.data));
-      }
-      fetchData();
-  });
-  console.log(data);
+  // useEffect(() => {
+  //     const fetchData = async () => {
+  //         axios.get('https://jsonplaceholder.typicode.com/posts')
+  //         .then(response => setData(response.data));
+  //     }
+  //     fetchData();
+  // });
+  // console.log(data);
 
   return (
     <div className="App">
-      <ExportToExcel  apiData={data} fileName={fileName} />
+      <ExportToExcel   fileName={fileName} />
     </div>
   );
 }
--- src/ExportToExcel.js
+++ src/ExportToExcel.js
@@ -1,20 +1,42 @@
 import * as FileSaver from "file-saver";
 import * as XLSX from "xlsx";
-
-export const ExportToExcel = ({ apiData, fileName }) => {
+import jsonData from "./file.json";
+export const ExportToExcel = ({fileName }) => {
   const fileType =
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
   const fileExtension = ".xlsx";
 
-  const exportToCSV = (apiData, fileName) => {
-    const ws = XLSX.utils.json_to_sheet(apiData);
+  const exportToCSV = (jsonData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(jsonData);
     const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
     const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
     const data = new Blob([excelBuffer], { type: fileType });
     FileSaver.saveAs(data, fileName + fileExtension);
   };
 
+  const onChange = async (event) => {
+    if (event.target.files) {
+      const parsedData = await readJsonFile(event.target.files[0])
+
+      console.log(parsedData)
+    }
+  }
+
+  const readJsonFile = (file) =>
+    new Promise((resolve, reject) => {
+      const fileReader = new FileReader()
+  
+      fileReader.onload = event => {
+        if (event.target) {
+          resolve(JSON.parse(event.target.result))
+        }
+      }
+  
+      fileReader.onerror = error => reject(error)
+      fileReader.readAsText(file)
+    })
+  
   return (
-    <button onClick={(e) => exportToCSV(apiData, fileName)}>Export</button>
+    <input type="file" accept=".json,application/json" onChange={onChange} />
   );
 };
